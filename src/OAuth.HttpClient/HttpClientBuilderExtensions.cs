a6f0525ac94147224170577264b38b8d475ec2c9
Duncan Roosma <github@roosma.me>
2021-02-17 10:23:37 +0100

    Adds HttpClientBuilder .WithOAuth extension (#3)

---
+++ src/OAuth.HttpClient/HttpClientBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OAuth.HttpClient
+{
+    public static class HttpClientBuilderExtensions
+    {
+        public static IHttpClientBuilder WithOAuth(this IHttpClientBuilder builder, Settings settings)
+            => builder.WithOAuth(provider => new OAuthAuthenticator(() => provider.GetRequiredService<IHttpClientFactory>().CreateClient(), settings));
+
+        public static IHttpClientBuilder WithOAuth(this IHttpClientBuilder builder, Authentication authentication)
+            => builder.AddHttpMessageHandler(_ => new AuthenticatedRequestHandler(authentication));
+
+        public static IHttpClientBuilder WithOAuth(this IHttpClientBuilder builder, Func<IServiceProvider, Authentication> authenticationFactory)
+            => builder.AddHttpMessageHandler(provider => new AuthenticatedRequestHandler(authenticationFactory.Invoke(provider)));
+    }
+}
