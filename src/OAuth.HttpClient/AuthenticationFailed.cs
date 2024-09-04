42a7e7106277dbf84e1a02d8d445caf3dd4f0ee5
Duncan Roosma <github@roosma.me>
2024-09-04 20:05:48 +0200

    remove some warnings and remove older net versions

--- src/OAuth.HttpClient/AuthenticationFailed.cs
+++ src/OAuth.HttpClient/AuthenticationFailed.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Net.Http;
-using System.Runtime.Serialization;
 
 namespace OAuth.HttpClient
 {
     [Serializable]
     public class AuthenticationFailed : Exception
     {
-        protected AuthenticationFailed(SerializationInfo info, StreamingContext context) 
-            : base(info, context)
-        {
-        }
-
         private AuthenticationFailed(HttpResponseMessage responseMessage,
                                      Settings settings)
             : base($"authentication failed for `{settings.ClientId}` with scope {settings.Scope} reason {responseMessage.ReasonPhrase}")
--- src/OAuth.HttpClient/Settings.cs
+++ src/OAuth.HttpClient/Settings.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
-using System.Reflection.Metadata;
 
 namespace OAuth.HttpClient
 {
@@ -34,27 +30,5 @@ namespace OAuth.HttpClient
         /// The margin used for refreshing the token before given expiry time
         /// </summary>
         public TimeSpan ExpireMargin { get; }
-    }
-
-    public class SettingsDto
-    {
-        public string ClientId { get; set; } = string.Empty;
-        public string ClientSecret { get; set; } = string.Empty;
-        public IEnumerable<string> Scopes { get; set; } = Array.Empty<string>();
-        public string TokenEndpoint { get; set; } = string.Empty;
-
-        /// <summary>
-        /// The margin in seconds used for refreshing the token before given expiry time
-        /// </summary>
-        public int? ExpireMarginInSeconds { get; } = 30;
-
-        public static implicit operator Settings(SettingsDto dto)
-            => new(
-                dto.ClientId,
-                dto.ClientSecret,
-                string.Join(" ", dto.Scopes),
-                new Uri(dto.TokenEndpoint),
-                dto.ExpireMarginInSeconds.HasValue ? TimeSpan.FromSeconds(dto.ExpireMarginInSeconds.Value) : null
-                );
     }
 }
