c700ce2ee5da3b3f12a0ab1cb855e4eaf5f96e68
Duncan Roosma <github@roosma.me>
2024-09-04 18:02:35 +0200

    move settings out

---
+++ src/OAuth.HttpClient/SettingsDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OAuth.HttpClient
+{
+    public class SettingsDto
+    {
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+        public IEnumerable<string> Scopes { get; set; } = [];
+        public string TokenEndpoint { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The margin in seconds used for refreshing the token before given expiry time
+        /// </summary>
+        public int? ExpireMarginInSeconds { get; } = 30;
+
+        public static implicit operator Settings(SettingsDto dto)
+            => new(
+                   dto.ClientId,
+                   dto.ClientSecret,
+                   string.Join(" ", dto.Scopes),
+                   new Uri(dto.TokenEndpoint),
+                   dto.ExpireMarginInSeconds.HasValue ? TimeSpan.FromSeconds(dto.ExpireMarginInSeconds.Value) : null
+                  );
+    }
+}
